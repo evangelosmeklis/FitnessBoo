f8065bdc15e2dc1c4862496abda044c1005b985b
Evangelos Meklis <vmeklis@hotmail.com>
2025-07-23 13:51:48 +0300

    Initial Commit

---
+++ FitnessBoo/FitnessBooApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessBooApp.swift
+//  FitnessBoo
+//
+//  Created by Evangelos Meklis on 23/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessBooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessBooTests/FitnessBooTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessBooTests.swift
+//  FitnessBooTests
+//
+//  Created by Evangelos Meklis on 23/7/25.
+//
+
+import Testing
+@testable import FitnessBoo
+
+struct FitnessBooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
